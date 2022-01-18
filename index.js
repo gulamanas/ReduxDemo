ab241c9b6397693030d554d4e211e3cb1fea411b
Gulam Anas <g.anas786@gmail.com>
2022-01-18 20:10:49 +0530

    first redux code

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('From index.js')
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState())
+const updateStore = store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+updateStore()
